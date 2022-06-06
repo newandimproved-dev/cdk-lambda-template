f15095d49ef4708a862af24cc2b20d0fdc4cc80c
Mark Kang <newandimproved.dev@gmail.com>
2022-06-06 14:16:56 -0700

    Create bare bones maven lambda

---
+++ src/main/java/CdkLambdaTemplate/Handler.java
@@ -0,0 +1,14 @@
+package CdkLambdaTemplate;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class Handler implements RequestHandler<Map<String,String>, String>{
+
+  @Override
+    public String handleRequest(Map<String,String> event, Context context)
+    {
+      return new String("200 OK");
+    }
+  }
